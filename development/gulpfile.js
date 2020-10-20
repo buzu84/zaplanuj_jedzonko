94813e50097d4a657f1a8a5dbeaf6789caabcc53
Magdalena Buzikiewicz <buzu@op.pl>
2020-10-20 16:58:02 +0200

    App setup.

---
+++ development/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+gulp.task("sass", function() {
+    return gulp.src("scss/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(sass({
+        outputStyle: 'expended',
+        sourceComments: 'map'
+     }))
+    .pipe(gulp.dest("css"))
+
+});
+gulp.task("watch", function() {
+    gulp.watch("scss/*.scss", gulp.series("sass"))
+});
+
+
+
